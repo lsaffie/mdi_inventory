57367b50b427b4333ef59fadeaf6dd915a5e137e
lsaffie <lsaffie@luis-saffies-macbook.local>
2008-11-19 11:21:05 -0500

    adding user peter to the system

--- db/migrate/20081119154540_add_user_to_system.rb
+++ db/migrate/20081119154540_add_user_to_system.rb
@@ -1,9 +1,11 @@
 class AddUserToSystem < ActiveRecord::Migration
   def self.up
     User.create(:login => 'lsaffie', :email => 'lsaffie@ehealthinnovation.org', :password => 'foobar', :password_confirmation => 'foobar')
+    User.create(:login => 'peter', :email => 'peter.picton@uhn.on.ca', :password => 'foobar', :password_confirmation => 'foobar')
   end
 
   def self.down
     User.delete(User.find_by_login('lsaffie'))
+    User.delete(User.find_by_login('peter'))
   end
 end
